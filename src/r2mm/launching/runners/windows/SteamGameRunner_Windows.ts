33e5e3d72c62bca02a744308512ac39e2441bf81
Od1zee <128906315+Od1zee@users.noreply.github.com>
2023-12-04 13:24:45 +1000

    Update SteamGameRunner_Windows.ts

--- src/r2mm/launching/runners/windows/SteamGameRunner_Windows.ts
+++ src/r2mm/launching/runners/windows/SteamGameRunner_Windows.ts
@@ -9,7 +9,7 @@ import { exec } from 'child_process';
 import GameInstructions from '../../instructions/GameInstructions';
 import GameInstructionParser from '../../instructions/GameInstructionParser';
 
-export default class SteamGameRunner_Windows extends GameRunnerProvider {
+export default class GameRunner_Windows extends GameRunnerProvider {
 
     public async getGameArguments(game: Game, profile: Profile): Promise<string | R2Error> {
         const instructions = await GameInstructions.getInstructionsForGame(game, profile);
@@ -32,23 +32,22 @@ export default class SteamGameRunner_Windows extends GameRunnerProvider {
     async start(game: Game, args: string): Promise<void | R2Error> {
         return new Promise(async (resolve, reject) => {
             const settings = await ManagerSettings.getSingleton(game);
-            const steamDir = await GameDirectoryResolverProvider.instance.getSteamDirectory();
-            if (steamDir instanceof R2Error) {
-                return resolve(steamDir);
+            const gameExecutablePath = settings.getContext().gameSpecific.gameExecutablePath;
+            if (!gameExecutablePath) {
+                return resolve(new R2Error('Game executable path not set', '', 'Please set the game executable path in the settings'));
             }
 
-            const gameExecutablePath = 'path_to_your_game_executable';
+            LoggerProvider.instance.Log(LogSeverity.INFO, `Game executable path is: ${gameExecutablePath}`);
+            LoggerProvider.instance.Log(LogSeverity.INFO, `Running command: ${gameExecutablePath} ${args} ${settings.getContext().gameSpecific.launchParameters}`);
 
-    LoggerProvider.instance.Log(LogSeverity.INFO, `Game executable path is: ${gameExecutablePath}`);
-    LoggerProvider.instance.Log(LogSeverity.INFO, `Running command: ${gameExecutablePath} ${args} ${settings.getContext().gameSpecific.launchParameters}`);
-
-    exec(`"${gameExecutablePath}" ${args} ${settings.getContext().gameSpecific.launchParameters}`, (err => {
-        if (err !== null) {
-            LoggerProvider.instance.Log(LogSeverity.ACTION_STOPPED, 'Error was thrown whilst starting modded');
-            LoggerProvider.instance.Log(LogSeverity.ERROR, err.message);
-            const r2err = new R2Error('Error starting game', err.message, 'Ensure that the game executable path has been set correctly');
-            return reject(r2err);
+            exec(`"${gameExecutablePath}" ${args} ${settings.getContext().gameSpecific.launchParameters}`, (err => {
+                if (err !== null) {
+                    LoggerProvider.instance.Log(LogSeverity.ACTION_STOPPED, 'Error was thrown whilst starting modded');
+                    LoggerProvider.instance.Log(LogSeverity.ERROR, err.message);
+                    const r2err = new R2Error('Error starting game', err.message, 'Ensure that the game executable path has been set correctly');
+                    return reject(r2err);
                 }
+                return resolve();
             }));
         });
     }
